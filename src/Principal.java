f057fc6d11d0c0ed743324f7c68bda7877d91fa2
imjowend <joaquinvcestudio@gmail.com>
2024-04-27 16:15:44 -0500

    Package

--- src/Principal.java
+++ src/Principal.java
@@ -3,9 +3,10 @@ import com.imjowend.screenmatch.Modelos.Pelicula;
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.evalua(10);
         miPelicula.evalua(8);
@@ -15,14 +16,5 @@ public class Principal {
         System.out.println(miPelicula.getTotalEvaluaciones());
 
         System.out.println(miPelicula.calculaMedia());
-
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1998;
-        otraPelicula.duracionEnMinutos = 180;
-
-        otraPelicula.muestraFichaTecnica();
-
-        System.out.println(otraPelicula.getTotalEvaluaciones());
     }
 }
