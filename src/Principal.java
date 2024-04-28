c9db420c56256bb2308c5b253401d0c130fdbbf1
imjowend <joaquinvcestudio@gmail.com>
2024-04-27 23:33:11 -0500

    Override para sobreescritura de metodos

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import com.imjowend.screenmatch.Modelos.Pelicula;
+import com.imjowend.screenmatch.Modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -16,5 +17,14 @@ public class Principal {
         System.out.println(miPelicula.getTotalEvaluaciones());
 
         System.out.println(miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del Dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
     }
 }
